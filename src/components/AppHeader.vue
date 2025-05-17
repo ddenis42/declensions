059f3f5fb07944891d4ce91ea6bcd2afa9dd31b3
Denis Demydovych <denis.demydovych@oracle.com>
2025-05-17 09:03:01 +0300

    Title responsiveness

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
   <v-app-bar>
-    <v-app-bar-title>Відмінювання українських імен <span style="color: gray">(посади, звання ЗСУ, ПІБ)</span></v-app-bar-title>
+    <v-app-bar-title>
+      Відмінювання українських імен
+      <span class="d-none d-sm-inline-block" style="color: gray">(посади, звання ЗСУ, ПІБ)</span>
+    </v-app-bar-title>
   </v-app-bar>
 </template>
